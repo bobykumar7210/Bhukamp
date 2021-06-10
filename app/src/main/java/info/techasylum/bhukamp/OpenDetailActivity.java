1bfc3fe31aa66edf99efaca36f0b4356851ccd9e
boby kumar <bobykumar.info7@gmail.com>
2021-06-10 15:58:18 +0530

    action bar hide OpendetailActivity

--- app/src/main/java/info/techasylum/bhukamp/OpenDetailActivity.java
+++ app/src/main/java/info/techasylum/bhukamp/OpenDetailActivity.java
@@ -22,6 +22,7 @@ public class OpenDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_detail);
+        getSupportActionBar().hide();
         mWebView = findViewById(R.id.webview);
 
         loaingSpinner = findViewById(R.id.loading_spinner_detail);
